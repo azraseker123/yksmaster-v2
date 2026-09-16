b9a8c7f18f34b027b094dd0d519df02f62e889b6
azraseker123 <azraseker630@gmail.com>
2026-09-17 00:57:27 +0300

    Add import for authVerifyEmail handler

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import authLogin from '../handlers/auth/login.js';
 import authLogout from '../handlers/auth/logout.js';
 import authMe from '../handlers/auth/me.js';
 import authRegister from '../handlers/auth/register.js';
+import authVerifyEmail from '../handlers/auth/verify-email.js';
 import authResetPassword from '../handlers/auth/reset-password.js';
 import badges from '../handlers/badges.js';
 import curriculum from '../handlers/curriculum.js';
