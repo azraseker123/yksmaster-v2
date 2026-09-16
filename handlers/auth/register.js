4769bdb8413aedaeaeca5dd988f0369832aba804
azraseker123 <azraseker630@gmail.com>
2026-09-17 01:30:09 +0300

    Clean up imports in register.js
    Removed unused imports from the register handler.

--- handlers/auth/register.js
+++ handlers/auth/register.js
@@ -2,9 +2,7 @@ import bcrypt from 'bcryptjs';
 import crypto from 'crypto';
 import { sendEmail } from '../../lib/email.js';
 import { query } from '../../lib/db.js';
-import { createSessionToken, sessionCookie } from '../../lib/auth.js';
-import { onlyMethods, text, int, normalizeEmail, publicUser } from '../../lib/http.js';
-import { accessFor } from '../../lib/plans.js';
+import { onlyMethods, text, int, normalizeEmail } from '../../lib/http.js';
 
 const TRACKS=new Set(['sayisal','esit_agirlik','sozel']);
 
